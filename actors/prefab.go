18055a39caac46d552ace137a63b4fa95577a353
Erich Newey <erich.newey@divvypay.com>
2020-01-27 08:08:41 -0700

    fix trampoline reaction

--- actors/prefab.go
+++ actors/prefab.go
@@ -13,17 +13,18 @@ func NewTrampoline(x, y, z int, sprite sprites.Spritemap) Actor {
 	rock := NewStaticActor(
 		"wall",
 		sprite,
-		colliders.NewBlock(x, y, z, 12, 8, 8, true, true, fmt.Sprintf("manual-rock")),
+		colliders.NewBlock(x, y, z, 12, 8, 8, true, true, fmt.Sprintf("manual-trampoline")),
 		-2, -8,
 	)
 	reaction := func(args ...interface{}) {
 		subject := args[0].(CanMove)
 		object := args[1].(Actor)
 
+		_, _, vz := subject.Vel()
 		_, _, sz := subject.Collider().Pos()
 		ox, oy, oz := object.Collider().Pos()
 		od := object.Collider().ZDepth(ox, oy)
-		if sz > oz+od {
+		if sz >= oz+od && vz < 0 {
 			events.Enqueue(events.New(1, 3, []interface{}{subject, 0.0, 0.0, 2.5}))
 		}
 	}
